7b330d059cdc943565d1cb9aaa7d6b34666f2645
Gabriel Duarte <gabriel160499@gmail.com>
2019-10-07 14:20:11 -0300

    Update example.js

--- example.js
+++ example.js
@@ -2,3 +2,11 @@ getText('jsapi').then(response => {
     console.log(response)
 });
 postText('jsapi', 'newApi')
+
+//Encrypt
+
+postText('path', 'Hello World' , true, 'password')
+
+getText('path',true,true,'password').then(response => {
+     console.log(response)
+});
